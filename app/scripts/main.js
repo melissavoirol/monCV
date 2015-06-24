3e3323ebce05dceb3c6bb86efa61d6ef993ca644
melissavoirol <melissa.voirol@he-arc.ch>
2015-06-24 17:03:00 +0200

    Charts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
 'use strict';
 $('a').smoothScroll();
+
+
+var doughnutData = [
+    {
+        value: 33,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+	/*{
+        value: 66,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },*/
+]
+
+var context = document.getElementById('skills').getContext('2d');
+var skillsChart = new Chart(context).Doughnut(doughnutData);
+
 });
