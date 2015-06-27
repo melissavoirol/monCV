e13b3e57393e1b1ef78fe64ce05d84957e6e25d1
melissavoirol <melissa.voirol@he-arc.ch>
2015-06-27 13:49:18 +0200

    Devoir 2
    Ma version définitive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
 	'use strict';
 	
+	//Défilement fluide sur tous les liens
 	$('a').smoothScroll();
 	
 	
+	//Récupère l'élément progress
 	var progress = $('.progress');
+	//Stocke cet élément dans une variable
 	var jqueryProgress = $(progress);
 	
+	//Pour chaque élément progress
 	$(jqueryProgress).each(function() {
 	
+		//Récupère l'id
 		var progressId = $(this).attr("id");
+		//Récupère la valeur
 		var progressValue = $(this).find('.progress-bar').attr('aria-valuenow');
 		
+		//Remplace l'élément progress (barre de progression) par un élément canvas (diagramme)
 		$(this).replaceWith('<canvas id = "' + progressId + '" width="100" height="100"></canvas>');
 				
+		//Récupère l'id de l'élément et le stock dans une variable
 		var context = document.getElementById(progressId).getContext('2d');
 		
+		//Les données du graphique
 		var data = [
 		{
 			value: progressValue,
@@ -27,6 +36,7 @@ $(document).ready(function(){
 		}
 		]
 
+		//Nouveau graphique selon données et différentes options ci-dessous
 		var myChart = new Chart(context).Doughnut(data, {
 			segmentShowStroke : false,
 			percentageInnerCutout : 80,
